2355066923a2f286568e99c602a85f211c45ff5f
tariqul islam shuvo <tariq10043@gmail.com>
2023-06-06 20:36:49 +0600

    testing env set up is completed

---
+++ src/test/product/Create.spc.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+describe('message', () => {
+    it('message', ()=>{
+        // expect(validation.checkEmailAddress('test@domain.com')).to.equal(true)
+    })
+
+    it('message', ()=>{
+        // expect(validation.checkEmailAddress('example.com')).to.equal(false)
+    })
+})
